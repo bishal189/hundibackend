9ab705e02c9cfbcfed231731c33e8578551b234d
bishal189 <bishalmurmu150@gmail.com>
2023-12-12 08:00:03 +0545

    update

--- hundi/settings.py
+++ hundi/settings.py
@@ -73,7 +73,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:5173",
      "https://hundi1.nelify.app",
-     "https://netlify.app",
 
      # Add other allowed origins as needed
  ]
